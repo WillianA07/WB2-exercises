7be7b92a8a0c2e6f4acaa22976754530f97fddcb
Willian Arenas <williamarenas07@gmail.com>
2022-10-18 11:06:15 -0400

    rule of 72

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,6 @@
+var savingAmount = 100;
+var interestRate = 25;
+var amountDouble = 200;
+var time = 72 / (interestRate);
+
+console.log("At a " + interestRate + " interest rate, your savings account will be worth " + amountDouble + " in " + time)
