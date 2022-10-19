a3812eeebd35c741737ff34630f7fe77ded058d1
Willian Arenas <williamarenas07@gmail.com>
2022-10-19 09:36:53 -0400

    Conversion exercise

---
+++ NumericConversions/converion_tests.js
@@ -0,0 +1,49 @@
+// Description: This script tests various numeric 
+//              conversion techniques
+// Author: Willian A
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+var a1 = parseInt(a);
+var b1 = parseInt(b);
+var c1 = parseInt(c);
+var d1 = parseInt(d);
+
+console.log(a1);
+console.log(b1);
+console.log(c1);
+console.log(d1);
+
+var a2 = parseFloat(a);
+var b2 = parseFloat(b);
+var c2 = parseFloat(c);
+var d2 = parseFloat(d);
+
+console.log(a2);
+console.log(b2);
+console.log(c2);
+console.log(d2);
+
+var a3 = Number(a);
+var b3 = Number(b);
+var c3 = Number(c);
+var d3 = Number(d);
+
+console.log(a3);
+console.log(b3);
+console.log(c3);
+console.log(d3);
+
+var a4 = +a;
+var b4 = +b;
+var c4 = +c;
+var d4 = +d;
+
+console.log(a4);
+console.log(b4);
+console.log(c4);
+console.log(d4);
+
